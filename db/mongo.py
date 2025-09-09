59568bb9229ff6ff91272695c0e86da60487e2d5
Muhammad Anees <muhammadaneesrajpoot@gmail.com>
2025-09-10 00:25:16 +0500

    Reapply "feat: authentication"
    This reverts commit 09e6cf85c8c4d53a6560b863efd9ba49943c0e9d.

--- db/mongo.py
+++ db/mongo.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 import os
 
 MONGO_URI = os.getenv("DB_URI", "mongodb://localhost:27017")
@@ -7,3 +6,13 @@ DB_NAME = os.getenv("DB_NAME", "mydatabase")
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
+
+# Ensure indexes
+try:
+    db["users"].create_index("email", unique=True)
+    db["password_reset_tokens"].create_index("token", unique=True)
+    db["password_reset_tokens"].create_index("expires_at")
+    db["email_logs"].create_index([("email", 1), ("created_at", -1)])
+except Exception:
+    # Index creation errors are non-fatal for runtime
+    pass
